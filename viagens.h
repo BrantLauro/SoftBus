a6efe8d2fbb56d76e577a33d54655a362a39d89c
BrantLauro <brantlauro@gmail.com>
2022-12-13 23:28:37 -0300

    Tela de Viagens em construcao :construction:

--- viagens.h
+++ viagens.h
@@ -6,7 +6,13 @@
 #define SOFTBUS_VIAGENS_H
 
 struct viagens {
-
+    int NumeroViagem;
+    char LocalSaida[51];
+    char LocalDestino[51];
+    char DiaSaida[51];
+    char HoraSaida[51];
+    double Quilometragem;
+    double Preco;
 };
 typedef struct viagens Viagens;
 
