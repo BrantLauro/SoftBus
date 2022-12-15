4292e4348342eec3e2a978723eda2eb4e7a26bc2
BrantLauro <brantlauro@gmail.com>
2022-12-14 21:52:59 -0300

    Interface do menu principal alterada :lipstick:

--- main.c
+++ main.c
@@ -2,13 +2,12 @@
 #include "interface.h"
 #include "viagens.h"
 #include "clientes.h"
-#include "configuracoes.h"
-#include "vendas.h"
+//#include "configuracoes.h"
+//#include "vendas.h"
 
-char opcoes[][51] = {"Cadastrar Viagem", "Pesquisar Viagem", "Realizar Venda",
-                     "Cadastrar Cliente", "Excluir Cliente", "Configuracoes"};
-int x[] = {21, 51, 82, 20, 52, 83};
-int y[] = {13, 13, 13, 18, 18, 18};
+char opcoes[][51] = {"Viagens",  "Clientes", "Vendas", "Fechar" };
+int x[] = {18, 43, 68, 93};
+int y[] = {24, 24, 24, 24};
 
 void TelaPrincipal () {
     TextColoreback(BROWN, BLACK);
@@ -18,32 +17,40 @@ void TelaPrincipal () {
     GotoXY(34, 3); printf("_____ \\ _  __ \\__  /_  _  __/__  __  |_  / / /__  ___/");
     GotoXY(34, 4); printf("____/ / / /_/ /_  __/  / /_  _  /_/ / / /_/ / _(__  ) ");
     GotoXY(34, 5); printf("/____/  \\____/ /_/     \\__/  /_____/  \\__,_/  /____/");
-    Borda(18, 12, 20, 2, 0,0);
-    Borda(48, 12, 20, 2, 0,0);
-    Borda(78, 12, 20, 2, 0,0);
-    Borda(18, 17, 20, 2, 0,0);
-    Borda(48, 17, 20, 2, 0,0);
-    Borda(78, 17, 20, 2, 0,0);
+    Borda(13, 23, 15, 2, 0,0);
+    Borda(38, 23, 15, 2, 0,0);
+    Borda(63, 23, 15, 2, 0,0);
+    Borda(88, 23, 15, 2, 0,0);
+    GotoXY(28, 9); printf(" .-------------------------------------------------------------.");
+    GotoXY(28, 10); printf("'------..-------------..----------..----------..----------..--.|");
+    GotoXY(28, 11); printf("|       \\\\            ||          ||          ||          ||  ||");
+    GotoXY(28, 12); printf("|        \\\\           ||          ||          ||          ||  ||");
+    GotoXY(28, 13); printf("|    ..   ||  _    _  ||    _   _ || _    _   ||    _    _||  ||");
+    GotoXY(28, 14); printf("|    ||   || //   //  ||   //  // ||//   //   ||   //   //|| /||");
+    GotoXY(28, 15); printf("|_.------\"''----------''----------''----------''----------''--'|");
+    GotoXY(28, 16); printf(" |)|      |       |       |       |    |      R&B|      ||==|  |");
+    GotoXY(28, 17); printf(" | |      |  _-_  |       |       |    |  .-.    |      ||==| C|");
+    GotoXY(28, 18); printf(" | |  __  |.'.-.' |   _   |   _   |    |.'.-.'.  |  __  | \"__=='");
+    GotoXY(28, 19); printf(" '---------'|( )|'----------------------'|( )|'----------\"\"");
+    GotoXY(28, 20); printf("             '-'                          '-'");
 }
 
 int main() {
     int escolha = 0;
     do {
         TelaPrincipal();
-        escolha = Menu(opcoes, x, y, escolha, 6);
+        escolha = Menu(opcoes, x, y, escolha, 4);
         if(escolha == 0){
             AtivarViagens();
         }
         if(escolha == 1) {
-        }
-        if(escolha == 2) {
-        }
-        if(escolha == 3) {
             AtivarClientes();
         }
-        if(escolha == 4) {
+        if(escolha == 2) {
+            //AtivarVendas();
         }
-        if(escolha == 5){
+        if(escolha == 3) {
+            break;
         }
     } while(escolha != -1);
 
